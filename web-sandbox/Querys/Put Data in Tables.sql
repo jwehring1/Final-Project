f18e08da7a82a87787ff9b925af6d6d06bf029b6
jCognizant <51711221+jCognizant@users.noreply.github.com>
2019-07-12 09:15:14 -0500

    Updated SQL Query

--- web-sandbox/Querys/Put Data in Tables.sql
+++ web-sandbox/Querys/Put Data in Tables.sql
@@ -1,9 +1,9 @@
 use addressbook;
--- INSERT INTO Address ( Street, City, State, Zipcode)
--- VALUES ('1234 Made up ln','Houston','TX',89327);
+INSERT INTO Address ( Street, City, State, Zipcode)
+VALUES ('1234 Made up ln','Houston','TX',89327);
 
--- INSERT INTO Address (Street, City, State, Zipcode)
--- VALUES ('2452 Errors','Boston','MC',13452);
-select * from address;
--- INSERT INTO Person (PersonName, Age, SSN, Race, Height,Weight,AddressID)
--- VALUES ('Frank',22,'12245567','African American',134,123,1);
+INSERT INTO Address (Street, City, State, Zipcode)
+VALUES ('2452 Errors','Boston','MC',13452);
+-- select * from address;
+INSERT INTO Person (PersonName, Age, SSN, Race, Height,Weight,AddressID)
+VALUES ('Frank',22,'12245567','African American',134,123,1);
